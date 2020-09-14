7378a37c0c3927469839d36f6f2a064d4f478178
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-09-14 22:18:50 +0000

    added assertEqual program

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion passed: ${actual} matches ${expected}`);
+  } else {
+    console.log(`\u274C Assertion failed: ${actual} doesn't match ${expected}`);
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
