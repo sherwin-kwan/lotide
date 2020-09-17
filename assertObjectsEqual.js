c55b13f0a966df7b41bf94036869b0d3a274e197
Sherwin <sherwin.kwan92@gmail.com>
2020-09-16 21:16:54 -0700

    playing around with objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,19 +1,23 @@
 import eqObjects from './eqObjects.js';
+import { inspect } from 'util';
 
 const assertObjectsEqual = (actual, expected) => {
   if (eqObjects(actual, expected)) {
-    console.log(`\u2705 Assertion passed: ${actual} matches ${expected}`);
+    console.log(`\u2705 Assertion passed: ${inspect(actual)} matches ${inspect(expected)}`);
   } else {
-    console.log(`\u274C Assertion failed: Actual output was ${actual}, expected ${expected}`);
+    console.log(`\u274C Assertion failed: Actual output was ${JSON.stringify(actual)}, expected ${JSON.stringify(expected)}`);
   }
 };
 
 export default assertObjectsEqual;
 
 // TEST CODE
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
-// assertArraysEqual([1, 2.5, 3], [1, 2, 3]);
-// assertArraysEqual([1, "2", 3], [1, 2, 3]);
-// assertArraysEqual(1, [1, 2, 3]);
-// assertArraysEqual([],[]);
+assertObjectsEqual([1, 2, 3], [1, 2, 3]);
+assertObjectsEqual([1, 2, 3, 4], [1, 2, 3]);
+assertObjectsEqual([1, "2", 3], [1, 2, 3]);
+assertObjectsEqual([],[]);
+assertObjectsEqual({a: 1, b: 2},{a: 1, b: 2});
+assertObjectsEqual({a: 1, b: 2},{b: 2, a: 1});
+assertObjectsEqual({a: 1},{a: 2});
+
+// Expected: true, false, false, true, true, true, false
--- eqObjectsTests.js
+++ eqObjectsTests.js
@@ -5,7 +5,7 @@
 // Stretch version (V2) works for objects too (by calling eqObjects itself to compare two objects. Note that arrays are a special type of object so the code still
 // works for comparing arrays)
 // You may switch between V1 and V2 by changing line 32.
-// See: './eqObjectsTests.js' to have fun with these functions
+// Disclaimer: This only compares key-value pairs for objects. Objects are considered identical if they have the same key-value pairs.
 
 import assertEqual from './assertEqual.js'; // Only used for testing
 import eqArrays from './eqArrays.js';
@@ -58,3 +58,59 @@ const eqObjects = (actual, expected) => {
 }
 
 export default eqObjects;
+
+// TEST CODE
+
+console.log(`\nBegin testing with objects that only contain primitives`);
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+console.log(`\nNow checking if arrays within objects work`);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
+
+console.log(`\nNow checking if objects within objects work`); // This will only work if V2 is used
+
+const efgh = { e: 1, f: { g: 1, h: 2 } };
+const efgh2 = { f: { g: 1, h: 2 }, e: 1 };
+const eghf = { e: { g: 1, h: 2 }, f: 1 };
+
+assertEqual(eqObjects(efgh, efgh2), true);
+assertEqual(eqObjects(efgh, eghf), false);
+
+// Trying to figure out which other kinds of things in Javascript are objects:
+// ARRAYS
+
+console.log('\nWait, I can use eqObjects to compare arrays and it actually works!!?');
+assertEqual(eqObjects([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqObjects([1, 2, 3, 4], [1, 2, 3]), false);
+assertEqual(eqObjects([1, 2.5, 3], [1, 2, 3]), false);
+assertEqual(eqObjects([1, "2", 3], [1, 2, 3]), false);
+assertEqual(eqObjects(1, [1, 2, 3]), false);
+assertEqual(eqObjects([], []), true);
+
+// STRINGS
+
+console.log(`\nStrings are primitives, surely they can't be objects too, right?`);
+assertEqual(eqObjects('abc','abc'), true); // Actually works. Strings can apparently be converted into objects with key-value pairs.
+assertEqual(eqObjects('a new string','a new string'), true);
+assertEqual(eqObjects('misspellings','mispellings'), false);
+
+// NUMBERS
+
+console.log(`\nHow about numbers? Are they objects?`);
+assertEqual(eqObjects(1 + 1, 2), true);
+assertEqual(eqObjects(2 + 2, 22), false); // Returns True. Turns out, numbers can't be converted into objects with key-value pairs
+
+// GARBAGE IN, GARBAGE OUT
+console.log(`\nOK now I'm bored, let's play stupid games.`);
+assertEqual(eqObjects(new Date('2020-01-01'), Math), false); // Actually, it's True. Neither Date nor Math objects have any keys/values.
+assertEqual(eqObjects(new Error('You fail!'), function alwaysPasses() { return true; }), false); // Ditto for Errors and Functions.
