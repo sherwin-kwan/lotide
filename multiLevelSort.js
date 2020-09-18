79a39ab59377bb19fd38d0b1bd502913b6ff8269
Sherwin <sherwin.kwan92@gmail.com>
2020-09-18 16:32:45 -0700

    added a multi-level general sort function

--- multiLevelSort.js
+++ multiLevelSort.js
@@ -18,6 +18,7 @@ const multiTypeSort = (a, b) => {
 };
 
 // Testing sorting functions
+console.log('TESTING');
 const testArray = [1, 8, 9, 10, 11];
 testArray.sort();
 console.log(testArray, '<-- normal sort');
@@ -25,25 +26,25 @@ testArray.sort(multiTypeSort);
 console.log(testArray, '<-- custom sort');
 
 // Sort students list - hard coded for this specific example
+// 1. Have a first sorting algorithm that sorts by property name
+// 2. Have a second sorting algorithm that sorts by age
+// 3. If the first sorting algorithm isn't decisive, use the second sorting algorithm.
 
 students.sort((a, b) => {
-  if (a.name === b.name) {
-    return 0 - multiTypeSort(a.age, b.age); // Flipped because we want higher numbers first
+  if (a.name === b.name) { // Same name
+    return 0 - multiTypeSort(a.age, b.age); // Flipped because we want higher ages first
   } else /* Name is decisive */ {
     return (a.name < b.name) ? -1 : 1;
   }
 });
-console.log(students);
+console.log('Student list sorted to: ', students);
 
-// Multi-level sort
-// 1. Have a first sorting algorithm that sorts by property name
-// 2. Have a second sorting algorithm that sorts by age
-// 3. If the first sorting algorithm is decisive, use it; otherwise, use the second sorting algorithm as a tiebreak
+// Multi-level sort. More complex way to handle different sorting parameters.
 
 const multiLevelSort = (sort1, sort2 /* sort1 and sort2 are callbacks */) => {
   return function (a, b) {
     (sort1(a, b) === 0) ? sort2(a, b) : sort1(a, b);
-  };
+  }
 }
 
 const sortObjectsByName = (obj1, obj2) => {
@@ -53,3 +54,6 @@ const sortObjectsByName = (obj1, obj2) => {
 const sortObjectsByAge = (obj1, obj2) => {
   return multiTypeSort(obj1['age'], obj2['age']);
 }
+
+console.log(`Now let's experiment with a chain of callbacks.`);
+console.log(students.sort(multiLevelSort(sortObjectsByName, sortObjectsByAge)));
